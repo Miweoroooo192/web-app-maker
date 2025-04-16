32bae1f5b7ef0dec3481ddfc94f5aac8230081d5
Miweoroooo192 <miweoro@outlook.pt>
2025-04-16 19:13:26 +0100

    Add files via upload

---
+++ appmakertest1/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        website = New TextBox()
+        title = New TextBox()
+        Label3 = New Label()
+        Button1 = New Button()
+        Edge = New RadioButton()
+        Chrome = New RadioButton()
+        Label4 = New Label()
+        port = New TextBox()
+        RelayBoings = New Button()
+        Path = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(32, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(70, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Directory"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(32, 106)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(286, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Website (how will have app will look like):"
+        ' 
+        ' website
+        ' 
+        website.Location = New Point(42, 142)
+        website.Name = "website"
+        website.Size = New Size(257, 27)
+        website.TabIndex = 2
+        ' 
+        ' title
+        ' 
+        title.Location = New Point(47, 222)
+        title.Name = "title"
+        title.Size = New Size(252, 27)
+        title.TabIndex = 3
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(47, 181)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(41, 20)
+        Label3.TabIndex = 4
+        Label3.Text = "Title:"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(619, 357)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 5
+        Button1.Text = "Make it!"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Edge
+        ' 
+        Edge.AutoSize = True
+        Edge.Location = New Point(448, 118)
+        Edge.Name = "Edge"
+        Edge.Size = New Size(92, 24)
+        Edge.TabIndex = 6
+        Edge.TabStop = True
+        Edge.Text = "Use Edge"
+        Edge.UseVisualStyleBackColor = True
+        ' 
+        ' Chrome
+        ' 
+        Chrome.AutoSize = True
+        Chrome.Location = New Point(448, 148)
+        Chrome.Name = "Chrome"
+        Chrome.Size = New Size(286, 24)
+        Chrome.TabIndex = 7
+        Chrome.TabStop = True
+        Chrome.Text = "Use Chrome/Chromium based browser"
+        Chrome.UseVisualStyleBackColor = True
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(47, 270)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(703, 20)
+        Label4.TabIndex = 8
+        Label4.Text = "Port (optional but do not blank it also it might be useful of you have another app created with this thing):"
+        ' 
+        ' port
+        ' 
+        port.Location = New Point(47, 318)
+        port.Name = "port"
+        port.Size = New Size(252, 27)
+        port.TabIndex = 9
+        port.Text = "8080"
+        ' 
+        ' RelayBoings
+        ' 
+        RelayBoings.Location = New Point(326, 64)
+        RelayBoings.Name = "RelayBoings"
+        RelayBoings.Size = New Size(94, 29)
+        RelayBoings.TabIndex = 11
+        RelayBoings.Text = "Browse..."
+        RelayBoings.UseVisualStyleBackColor = True
+        ' 
+        ' Path
+        ' 
+        Path.AutoSize = True
+        Path.Location = New Point(47, 64)
+        Path.Name = "Path"
+        Path.Size = New Size(148, 20)
+        Path.TabIndex = 12
+        Path.Text = "Deafult: Temp Folder"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Path)
+        Controls.Add(RelayBoings)
+        Controls.Add(port)
+        Controls.Add(Label4)
+        Controls.Add(Chrome)
+        Controls.Add(Edge)
+        Controls.Add(Button1)
+        Controls.Add(Label3)
+        Controls.Add(title)
+        Controls.Add(website)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Web App Maker"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents website As TextBox
+    Friend WithEvents title As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Edge As RadioButton
+    Friend WithEvents Chrome As RadioButton
+    Friend WithEvents Label4 As Label
+    Friend WithEvents port As TextBox
+    Friend WithEvents RelayBoings As Button
+    Friend WithEvents Path As Label
+
+End Class
---
+++ appmakertest1/Form1.vb
@@ -0,0 +1,171 @@
+﻿Imports System.Windows.Forms
+Imports System.IO
+Imports System.Net
+Imports System.Diagnostics.CodeAnalysis
+Imports System.Diagnostics.Metrics
+Public Class Form1
+    Dim n As String = Environment.NewLine
+    Dim folderDialog As New FolderBrowserDialog()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Define the command to check Python version
+        ' Define the command to check Python version
+        Dim processStartInfo As New ProcessStartInfo()
+        processStartInfo.FileName = "cmd.exe"
+        processStartInfo.Arguments = "/c py --version"
+        processStartInfo.RedirectStandardOutput = True
+        processStartInfo.UseShellExecute = False
+        processStartInfo.CreateNoWindow = True
+
+        Dim process As Process = Process.Start(processStartInfo)
+        Dim output As String = process.StandardOutput.ReadToEnd()
+        process.WaitForExit()
+
+        ' Display the result in a Label
+        If output.Contains("Python") Then
+            MessageBox.Show($"Python is installed! Version: {output}", "Python Version Check")
+        Else
+            MessageBox.Show($"Please install the lastest version of Python.", "Python Version Check")
+        End If
+    End Sub
+    Private Sub RelayBoings_Click(sender As Object, e As EventArgs) Handles RelayBoings.Click
+
+        folderDialog.Description = "Select a folder to save your files"
+        folderDialog.ShowNewFolderButton = True
+
+        ' Show the dialog to the user
+        If folderDialog.ShowDialog() = DialogResult.OK Then
+            Try
+                ' Retrieve the selected folder path
+                Dim folderPath As String = folderDialog.SelectedPath
+
+                ' Example: Specify a file to save within the selected folder
+                Dim content As String = "This is the content of your file!"
+
+                ' Write the content to a file in the selected folder
+                '
+
+                ' Notify the user of successful operation
+                Path.Text = folderPath
+            Catch ex As Exception
+                ' Handle any errors during the file save
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        Else
+            ' Notify user that the folder selection was canceled
+
+        End If
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim content = $"import eel{Environment.NewLine}eel.init('web'){Environment.NewLine}eel.start('index.html', port='{port.Text}')"
+        Dim temp = IO.Path.GetTempPath()
+        Dim folderPath = $"{temp}\{title.Text}"
+        Dim filePath = $"{temp}\{title.Text}\app.py"
+        If Not String.IsNullOrEmpty(folderDialog.SelectedPath) Then
+
+            folderPath = $"{folderDialog.SelectedPath}"
+            filePath = $"{folderDialog.SelectedPath}\app.py"
+
+        Else
+
+        End If
+
+        Dim packageName = "eel" ' Replace this with the package you want to check
+        Dim packageName2 = "pyinstaller"
+        ' Create a process to run the Python command
+        Dim processStartInfo As New ProcessStartInfo
+        processStartInfo.FileName = "cmd.exe"
+        processStartInfo.Arguments = $"/c py -m pip show {packageName}"
+        processStartInfo.RedirectStandardOutput = True
+        processStartInfo.UseShellExecute = False
+        processStartInfo.CreateNoWindow = True
+
+        Dim process As Process = Process.Start(processStartInfo)
+        Dim output = process.StandardOutput.ReadToEnd
+        process.WaitForExit()
+
+        If Edge.Checked Then
+            content = $"import eel{Environment.NewLine}eel.init('web'){Environment.NewLine}eel.start('index.html', mode='edge', port='{port.Text}')"
+        ElseIf Chrome.Checked Then
+            content = $"import eel{Environment.NewLine}eel.init('web'){Environment.NewLine}eel.start('index.html', mode='chrome', port='{port.Text}')"
+        Else
+            MessageBox.Show("Please select a option.", "App Maker", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Environment.Exit(0)
+        End If
+
+
+        ' Check if the package is installed
+        If output.Contains("Name:") Then
+            Dim nope = 69
+
+        ElseIf output.Contains("Name:") Then
+            Dim procesStartInfo As New ProcessStartInfo
+            procesStartInfo.FileName = "cmd.exe"
+            procesStartInfo.Arguments = "/c py -m pip install eel && py -m pip install pyinstaller"
+            procesStartInfo.RedirectStandardOutput = False
+            procesStartInfo.UseShellExecute = True
+            procesStartInfo.CreateNoWindow = False
+
+            Dim proces = Process.Start(processStartInfo)
+            Dim outut = process.StandardOutput.ReadToEnd
+            process.WaitForExit()
+            MessageBox.Show($"The packages 'eel' and 'pyinstaller' are installed!", "Python Package Check", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+
+        Try
+            ' Create the directory if it doesn't exist
+            Dim directoryPath = IO.Path.GetDirectoryName(filePath)
+            If Not IO.Directory.Exists(directoryPath) Then
+                IO.Directory.CreateDirectory(directoryPath)
+            End If
+            Dim directoryPath2 = $"{folderPath}\web"
+            If Not IO.Directory.Exists(directoryPath2) Then
+
+                IO.Directory.CreateDirectory(directoryPath2)
+                MessageBox.Show("File created successfully with the provided content!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+            If Not File.Exists($"{folderPath}\web\index.html") Then
+                Dim contet = $"<!DOCTYPE html><html><head><title>{title.Text}</title>
+
+  </head>
+  <body>
+    <script src='trollbox.js'></script>
+    <h1>Restart the App of it doenst automatically redirect to {title.Text}</h1>
+  </body>
+</html>"
+
+                Dim htmlfilePath = $"{folderPath}\web\index.html"
+                File.WriteAllText(htmlfilePath, contet)
+                MessageBox.Show("File created successfully with the provided content!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+            If Not File.Exists($"{folderPath}\web\trollbox.js") Then
+                Dim contete = $"location.href = '{website.Text}';"
+                Dim JSfilePath = $"{folderPath}\web\trollbox.js"
+                File.WriteAllText(JSfilePath, contete)
+                MessageBox.Show("File created successfully with the provided content!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+            ' Write the content to the file
+            File.WriteAllText(filePath, content)
+            MessageBox.Show("File created successfully with the provided content!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Try
+                Dim procestartInfo As New ProcessStartInfo
+                procestartInfo.FileName = "cmd.exe"
+                procestartInfo.Arguments = $"/c cd {folderPath} && py -m eel app.py web --onefile --noconsole"
+                procestartInfo.RedirectStandardOutput = False
+                procestartInfo.UseShellExecute = True
+                procestartInfo.CreateNoWindow = False
+                Dim dimdimdim = process.Start(procestartInfo)
+
+                MessageBox.Show($"The application was successfully packaged and executed! (Folder Source: {folderPath} )", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            Catch ex As Exception
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        Catch ex As Exception
+            ' Handle any errors (e.g., permission issues)
+            MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+End Class
